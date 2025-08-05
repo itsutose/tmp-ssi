f533d769e2fb2ed06e42788f3085423d7e3d2027
沓名海斗 <kutsuna@d-vme3v1.put>
2025-08-05 13:16:30 +0900

    refactor: ECRをContainerRegistryConstructに高抽象化し環境対応

--- src/main-infra/construct/ecr.ts
+++ src/main-infra/construct/ecr.ts
@@ -1,17 +1,30 @@
 import { Construct } from "constructs";
 import { IRepository, Repository } from "aws-cdk-lib/aws-ecr";
-import { REPOSITORY_NAME } from "../../shared/enviroment/common";
 import { ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { PROJECT_CONFIG, ContainerRegistryProps } from "../../shared/enviroment/common";
 
-export class EcrConstruct extends Construct {
+/**
+ * コンテナレジストリを管理するConstruct
+ * RAGアプリケーション用のECRリポジトリとアクセス権限を提供
+ */
+export class ContainerRegistryConstruct extends Construct {
     public readonly repository: IRepository;
 
-    constructor(scope: Construct, id: string) {
+    constructor(scope: Construct, id: string, props: ContainerRegistryProps) {
         super(scope, id);
 
-        const projectRAGEcr = Repository.fromRepositoryName(this, "ProjectRAGEcr", REPOSITORY_NAME);
+        const repositoryName = props.repositoryName ?? PROJECT_CONFIG.RESOURCES.ECR.REPOSITORY_NAME;
+        
+        // 既存のECRリポジトリを参照（環境名は含めない）
+        const ragRepository = Repository.fromRepositoryName(
+            this, 
+            "RagRepository", 
+            repositoryName
+        );
 
-        projectRAGEcr.grantPull(new ServicePrincipal("lambda.amazonaws.com"));
-        this.repository = projectRAGEcr;
+        // Lambda サービスにpull権限を付与
+        ragRepository.grantPull(new ServicePrincipal("lambda.amazonaws.com"));
+        
+        this.repository = ragRepository;
     }
 }
