2d77cfbeea3e0c01bf0268a9a35fe8ce6912fb11
沓名海斗 <kutsuna@d-vme3v1.put>
2025-08-05 13:18:13 +0900

    feat: RAGシステム用の統合インフラストラクチャスタックを実装

---
+++ src/main-infra/stack/infra-stack.ts
@@ -0,0 +1,63 @@
+import { Construct } from "constructs";
+import { Stack, StackProps } from "aws-cdk-lib";
+import { PROJECT_CONFIG, EnvironmentProps } from "../../shared/enviroment/common";
+import { ContainerRegistryConstruct } from "../construct/ecr";
+import { RagProcessingConstruct } from "../construct/lambda";
+import { DynamoDBConstruct } from "../construct/dynamoDB";
+
+export interface RagInfraStackProps extends StackProps {
+  readonly environment: string;
+}
+
+/**
+ * RAGシステムのメインインフラストラクチャスタック
+ * コンテナレジストリ、Lambda関数、DynamoDBテーブルを組み合わせてRAGシステムを構築
+ */
+export class RagInfraStack extends Stack {
+  public readonly containerRegistry: ContainerRegistryConstruct;
+  public readonly dataStorage: DynamoDBConstruct;
+  
+  // Lambda関数群
+  public readonly ragProcessor: RagProcessingConstruct;
+
+  constructor(scope: Construct, id: string, props: RagInfraStackProps) {
+    super(scope, id, props);
+
+    // コンテナレジストリの構築
+    this.containerRegistry = new ContainerRegistryConstruct(this, "ContainerRegistry", {
+      environment: props.environment,
+    });
+
+    // データストレージの構築
+    this.dataStorage = new DynamoDBConstruct(this, "DataStorage");
+
+    // Lambda関数群の構築
+    this.ragProcessor = new RagProcessingConstruct(this, "RagProcessor", {
+      environment: props.environment,
+      imageRepository: this.containerRegistry.repository,
+      documentTable: this.dataStorage.table,
+    });
+
+    // 出力値の定義
+    this.exportValues(props.environment);
+  }
+
+  /**
+   * スタックの重要な値をエクスポート
+   */
+  private exportValues(environment: string): void {
+    // Lambda関数名をエクスポート
+    // new CfnOutput(this, "RagFunctionName", {
+    //   value: this.ragProcessing.function.functionName,
+    //   description: "RAG processing Lambda function name",
+    //   exportName: `${PROJECT_CONFIG.PROJECT_NAME}-${environment}-rag-function-name`,
+    // });
+
+    // ECRリポジトリURIをエクスポート
+    // new CfnOutput(this, "EcrRepositoryUri", {
+    //   value: this.containerRegistry.repository.repositoryUri,
+    //   description: "ECR repository URI for container images",
+    //   exportName: `${PROJECT_CONFIG.PROJECT_NAME}-${environment}-ecr-uri`,
+    // });
+  }
+}
