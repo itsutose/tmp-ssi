9a2a434642f7036b3ebb5dc8d6550d1008b08ad0
沓名海斗 <kutsuna@d-vme3v1.put>
2025-08-12 11:05:41 +0900

    feat: Snapshotの作成 1. jestの環境設定 2. サンプルテストの作成 3. .gitignoreにスナップショットが含まれないように調整

---
+++ test/infrapipeline-stack.test.ts
@@ -0,0 +1,17 @@
+import { App } from "aws-cdk-lib";
+import { InfraPipelineStack } from "../src/pipeline/cdk-pipeline";
+import { Template, Match } from "aws-cdk-lib/assertions";
+
+describe("InfraPipelineStack", () => {
+    test('Snapshot test', () => {
+        const app = new App();
+        const stack = new InfraPipelineStack(app, "InfraPipelineStack", {
+            env: {
+                account: process.env.CDK_DEFAULT_ACCOUNT,
+                region: process.env.CDK_DEFAULT_REGION,
+            },
+        });
+        const template = Template.fromStack(stack);
+        expect(template.toJSON()).toMatchSnapshot();
+    });
+});
