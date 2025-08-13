f6f16fe7515498534859cdc829e52ddfb87608fa
沓名海斗 <kutsuna@d-vme3v1.put>
2025-08-13 17:38:59 +0900

    pipeline上でのSFとLambdaのデプロイ

--- src/pipeline/cdk-pipeline.ts
+++ src/pipeline/cdk-pipeline.ts
@@ -1,23 +1,84 @@
 import { Construct } from "constructs";
 import { Stack, StackProps } from "aws-cdk-lib";
-import { LAMBDA_FUNCTION_NAME, REPOSITORY_NAME } from "../shared/enviroment/common";
+import { API_LAMBDA_FUNCTION_NAME, stepFunctionDefinition } from "../shared/enviroment/common";
 import { EcrConstruct } from "../main-infra/construct/ecr";
-import { LambdaImageConstruct } from "../main-infra/construct/lambda";
-import { Architecture } from "aws-cdk-lib/aws-lambda";
+import { LambdaImageConstruct, Lambda } from "../main-infra/construct/lambda";
+import { Architecture, Code } from "aws-cdk-lib/aws-lambda";
+import { StepFunctions } from "../main-infra/construct/stepfunctions";
+import { Role } from "aws-cdk-lib/aws-iam";
+import { ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { PolicyDocument } from "aws-cdk-lib/aws-iam";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class InfraPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props);
 
+        // ECRを用いたLambdaによるFastAPIのデプロイ
         const repository = new EcrConstruct(this, "EcrConstruct");
 
-        const lambdaProps = {
-            functionName: LAMBDA_FUNCTION_NAME,
+        const lambdaImageConstructProps = {
+            functionName: API_LAMBDA_FUNCTION_NAME,
             imageTag: "latest",
             repository: repository.repository,
             architecture: Architecture.ARM_64,
         }
 
-        const lambda = new LambdaImageConstruct(this, "LambdaImageConstruct", lambdaProps);
+        const lambda = new LambdaImageConstruct(this, "LambdaImageConstruct", lambdaImageConstructProps);
+
+        // チェック用StepFunctionsのデプロイ
+        const stepFunctionsRole = new Role(this, "StepFunctionsRole", {
+            assumedBy: new ServicePrincipal("states.amazonaws.com"),
+            inlinePolicies: {
+                stepFunctionsPolicy: new PolicyDocument({
+                    statements: [
+                        new PolicyStatement({
+                            actions: ["lambda:InvokeFunction"],
+                            resources: [lambda.lambda.functionArn],
+                        }),
+                    ],
+                }),
+            },
+        });
+
+        const stepFunction = new StepFunctions(this, "StepFunctionsConstruct", {
+            stateMachineName: "sony-sonpo-rag-check-state-machine",
+            definitionBody: stepFunctionDefinition,
+            role: stepFunctionsRole,
+        });
+
+        // invoke用Lambda関数のIAMロール
+        const invokeLambdaRole = new Role(this, "InvokeLambdaRole", {
+            assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+            inlinePolicies: {
+                stepFunctionsInvokePolicy: new PolicyDocument({
+                    statements: [
+                        new PolicyStatement({
+                            actions: ["states:StartExecution"],
+                            resources: [stepFunction.stateMachine.stateMachineArn],
+                        }),
+                        new PolicyStatement({
+                            actions: [
+                                "logs:CreateLogGroup",
+                                "logs:CreateLogStream",
+                                "logs:PutLogEvents"
+                            ],
+                            resources: ["arn:aws:logs:*:*:*"],
+                        }),
+                    ],
+                }),
+            },
+        });
+
+        // invoke用Lambda関数
+        const invokeLambda = new Lambda(this, "InvokeStepFunctionsLambda", {
+            functionName: "sony-sonpo-invoke-stepfunctions",
+            code: Code.fromAsset("src/lambda-functions/invokeCheckStepFunctions"),
+            handler: "index.lambda_handler",
+            environment: {
+                ALLOWED_STATE_MACHINE_ARN: stepFunction.stateMachine.stateMachineArn,
+            },
+            role: invokeLambdaRole,
+        });
     }
 }
