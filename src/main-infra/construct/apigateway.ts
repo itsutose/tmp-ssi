678d407a9b9fa5ff22d520a746df200ff2938678
沓名海斗 <kutsuna@d-vme3v1.put>
2025-08-15 16:15:33 +0900

    apigwの認証の追加

--- src/main-infra/construct/apigateway.ts
+++ src/main-infra/construct/apigateway.ts
@@ -2,10 +2,14 @@ import { Construct } from "constructs";
 import { IRestApi, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { IFunction } from "aws-cdk-lib/aws-lambda";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { AuthorizationType } from "aws-cdk-lib/aws-apigateway";
+import { UserPool } from "aws-cdk-lib/aws-cognito";
+import { CognitoUserPoolsAuthorizer } from "aws-cdk-lib/aws-apigateway";
 
 export interface ApiGatewayProps {
     apiName: string;
     lambdaFunction: IFunction;
+    userPool: UserPool;
 }
 
 export class ApiGatewayConstruct extends Construct {
@@ -19,8 +23,18 @@ export class ApiGatewayConstruct extends Construct {
         const apiGateway = new RestApi(this, "ApiGateway", {
             restApiName: apiName,
         });
+        const auth = new CognitoUserPoolsAuthorizer(this, "CognitoUserPoolAuthorizer", {
+            cognitoUserPools: [props.userPool],
+        });
 
-        apiGateway.root.addMethod("GET", new LambdaIntegration(props.lambdaFunction));
+        apiGateway.root.addMethod(
+            "GET", 
+            new LambdaIntegration(props.lambdaFunction),
+            {
+                authorizer: auth,
+                authorizationType: AuthorizationType.COGNITO,
+            }
+        );
 
         this.apiGateway = apiGateway;
 
