f6d9b8b4a1a21ab5a5be08e2be422a2c6be9270f
yamaguchi <yamaguchi@scala-com.jp>
2025-08-18 17:25:24 +0900

    jest設定がpackageに統合されたので削除

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-// jest.config.js
-module.exports = {
-    preset: 'ts-jest', // Test実行前にts-jestでjsに変換
-    testEnvironment: 'node', // （ブラウザ環境ではなく）Node.js環境でテストを実行
-    testMatch: ['**/test/unit/**/*.test.ts'], // test/unit ディレクトリ内の .test.ts ファイルを対象にする
-};
